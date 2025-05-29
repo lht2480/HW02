de7ba0e0fa077539aecfae105eb6db95f75f34a8
lht2480 <jgjjgiggk@gmail.com>
2025-05-29 12:44:31 +0900

    create hw02 projrct

---
+++ HW02/HW02.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animals 
+{
+public:
+    virtual void Sound() = 0;
+};
+
+class Dog : public Animals 
+{
+public:
+    void Sound() 
+    {
+        cout << "Dog barks: Woof! Woof!" << endl;
+    }
+};
+
+class Cat : public Animals 
+{
+public:
+    void Sound() 
+    {
+        cout << "Cat meows: Meow! Meow!" << endl;
+    }
+};
+
+class Cow : public Animals
+{
+public:
+    void Sound()
+    {
+        cout << "Cow cries: Moo~ Moo~" << endl;
+    }
+};
+int main() 
+{
+    Dog d;
+    Cat c;
+    Cow Co;
+    Animals* animals[] = { &d,&c,&Co };
+
+    for (int i = 0; i < 3; i++)
+    {
+        animals[i]->Sound();
+    }
+
+    return 0;
+}
